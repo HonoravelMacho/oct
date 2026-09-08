501a5dd80e769a043ef5956f958f766aefea6b14
HonoravelMacho <tiagorabelo@example.com>
2026-09-08 14:38:38 -0300

    feat: tela cheia imersiva (esconde navegacao do Android)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Tela cheia imersiva: esconde a barra de navegação do Android
+  // (voltar/home/recentes). Ela só reaparece com gesto de borda e some
+  // sozinha em seguida.
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   final prefs = await SharedPreferences.getInstance();
   runApp(OctApp(prefs: prefs));
 }
